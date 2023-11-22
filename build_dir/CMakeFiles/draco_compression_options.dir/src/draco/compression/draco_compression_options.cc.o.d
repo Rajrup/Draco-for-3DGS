4f1c00ec05ad48bd25566d0a8d3c7d020538e921
YuanChun Sun <yuanjiun911@outlook.com>
2023-11-22 10:44:48 +0800

    finish all

--- build_dir/CMakeFiles/draco_compression_options.dir/src/draco/compression/draco_compression_options.cc.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/draco_compression_options.dir/src/draco/compression/draco_compression_options.cc.o: \
-  /Users/syjintw/Downloads/draco-1.5.6/src/draco/compression/draco_compression_options.cc \
-  /Users/syjintw/Downloads/draco-1.5.6/src/draco/compression/draco_compression_options.h \
-  /Users/syjintw/Downloads/draco-1.5.6/build_dir/draco/draco_features.h
